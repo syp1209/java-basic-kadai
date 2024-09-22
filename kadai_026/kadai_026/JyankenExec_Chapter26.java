fd67f583930de16dc008a584ce33325afb74f3b1
syp.1209 <47263084+syp1209@users.noreply.github.com>
2024-09-22 10:40:02 +0900

    じゃんけんアプリを作ろう　課題提出２
    課題を提出します。
    ご確認お願い致します。

---
+++ kadai_026/kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,12 @@
+package kadai_026;
+
+public class JyankenExec_Chapter26 {
+
+	public static void main(String[] args) {
+		Jyanken_Chapter26 jya = new Jyanken_Chapter26();
+		jya.playGame();
+
+
+	}
+
+}
---
+++ kadai_026/kadai_026/Jyanken_Chapter26.java
@@ -0,0 +1,80 @@
+package kadai_026;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter26 {
+	
+	Jyanken_Chapter26(){
+		System.out.println("自分のじゃんけんの手を入力しましょう");
+		System.out.println("グーはrockのrを入力しましょう");
+		System.out.println("チョキはscissorsのsを入力しましょう");
+		System.out.println("パーはpaperのpを入力しましょう");
+	}
+
+
+	//自分のじゃんけんの手を入力する
+	public String getMyChoice() {
+
+		//Scannerクラスのオブジェクトを生成する
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+
+			String input = scanner.next();
+			try {
+
+					if (!(input.equals("r")) && !(input.equals("s")) && !(input.equals("p"))) {
+						System.out.println("入力された値は不正です。再度入力してください");
+						continue;
+					}
+
+			} finally {
+			}	
+			scanner.close();
+			return input;
+		}
+
+	}
+
+
+	
+	//対戦相手のじゃんけんの手を乱数で選ぶ
+	public String getRandom() {
+		
+		HashMap<String,String> jyaMap = new HashMap<String,String>();
+		jyaMap.put("r","グー");
+		jyaMap.put("s","チョキ");
+		jyaMap.put("p","パー");
+		
+		
+		int Random = (int)Math.floor(Math.random() * 3);
+		if(Random == 1) {
+			return "r";
+		} else if(Random == 2) {
+			return "s";
+		}else {
+			return "p";
+		}
+		
+	}
+		
+	
+	//じゃんけんを行う
+	public void playGame() {
+		
+		String my = getMyChoice();
+		String partner = getRandom();
+		
+		System.out.println("自分の手は"+ my+","+"対戦相手の手は" + partner);
+
+	    if ((my.equals("r") && partner.equals("r")) || (my.equals("s") && partner.equals("s")) || (my.equals("p") && partner.equals("p"))) {
+	        System.out.println("あいこです");
+	    } else if ((my.equals("r") && partner.equals("s")) || (my.equals("s") && partner.equals("p")) || (my.equals("p") && partner.equals("r"))) {
+	        System.out.println("あなたの勝ちです");
+	    } else {
+	        System.out.println("あなたの負けです");
+	    }
+		
+	};
+
+}
